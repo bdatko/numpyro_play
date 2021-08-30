6833b2bf4dee9286c9dc4f07cb87d56274136aa2
bdatko <ben.datko@gmail.com>
2021-08-29 21:56:10 -0400

    feat(notebooks/Section\-3.6\-of\-Chapter\-3\--\-Inferences\-with\-binomials.*): add watermark to notebook notebooks/Section\ 3.6\ of\ Chapter\ 3\ -\ Inferences\ with\ binomials.*

--- notebooks/Section 3.6 of Chapter 3 - Inferences with binomials.py
+++ notebooks/Section 3.6 of Chapter 3 - Inferences with binomials.py
@@ -18,15 +18,27 @@
 # * the example was inspired from the post by [yongduek on the Pyro fourm](https://forum.pyro.ai/t/latent-categorical/2695/6?u=bdatko)
 # * If you need sometihng similar look at [Predictive](http://num.pyro.ai/en/latest/utilities.html#predictive) and [Example: Bayesian Models of Annotation](http://num.pyro.ai/en/latest/examples/annotation.html) from commit [Support infer_discrete for Predictive (#1086)](https://github.com/pyro-ppl/numpyro/commit/003424bb3c57e44b433991cc73ddbb557bf31f3c)
 
+# +
+import arviz as az
 import jax
 import jax.numpy as jnp
 import numpy as np
-import arviz as az
+import pandas as pd
+
 import numpyro
 import numpyro.distributions as dist
-from numpyro.infer.util import Predictive
-import pandas as pd
 from numpyro.infer import MCMC, NUTS, DiscreteHMCGibbs
+from numpyro.infer.util import Predictive
+# -
+
+# %matplotlib inline
+# %reload_ext autoreload
+# %autoreload 2
+# %load_ext watermark
+
+# %watermark -v -m -p arviz,jax,numpy,pandas,numpyro
+
+# %watermark -gb
 
 # +
 rng_key = jax.random.PRNGKey(2)
@@ -44,27 +56,30 @@ num_chains = 4
 # +
 def model36(a, b, probs, k=None):
     # priors: th, n
-    n = numpyro.sample('n', dist.Categorical(probs=probs))
-    th = numpyro.sample('th', dist.Beta(a, b))
+    n = numpyro.sample("n", dist.Categorical(probs=probs))
+    th = numpyro.sample("th", dist.Beta(a, b))
     # observation
     size = len(k)
-    with numpyro.plate(f'i=1..{size}', size=size):
-        obs = numpyro.sample('k', dist.Binomial(total_count=n, probs=th), obs=k)
-        
+    with numpyro.plate(f"i=1..{size}", size=size):
+        obs = numpyro.sample("k", dist.Binomial(total_count=n, probs=th), obs=k)
+
+
 a, b = 1, 1
 k = jnp.array([16, 18, 22, 25, 27])
 nmax = 500
-probs = jnp.array([1.]*nmax) / nmax
+probs = jnp.array([1.0] * nmax) / nmax
 # -
 
-numpyro.render_model(model36, (a,b,probs,k), render_distributions=True)
+numpyro.render_model(model36, (a, b, probs, k), render_distributions=True)
 
 kernel = numpyro.infer.DiscreteHMCGibbs(NUTS(model36), modified=True)
-mcmc = MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains)
+mcmc = MCMC(
+    kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains
+)
 mcmc.run(keys[0], a=b, b=b, probs=probs, k=k)
 mcmc.print_summary()
 
-ds= az.from_numpyro(mcmc)
+ds = az.from_numpyro(mcmc)
 az.plot_trace(ds);
 
 # ### Discrete `n`
@@ -73,19 +88,25 @@ az.plot_trace(ds);
 from numpyro.infer.util import Predictive
 
 kernel = NUTS(model36)
-mcmc = MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains)
+mcmc = MCMC(
+    kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains
+)
 mcmc.run(keys[1], a=b, b=b, probs=probs, k=k)
 mcmc.print_summary()
 
 az.plot_trace(mcmc.get_samples());
 
 posterior_samples = mcmc.get_samples()
-predictive = Predictive(model36, posterior_samples, infer_discrete=True, batch_ndims=num_chains)
+predictive = Predictive(
+    model36, posterior_samples, infer_discrete=True, batch_ndims=num_chains
+)
 discrete_samples = predictive(keys[2], *(a, b, probs, k))
 
-az.stats.summary(np.array(discrete_samples["n"].reshape(num_chains,num_samples)), hdi_prob=0.9)
+az.stats.summary(
+    np.array(discrete_samples["n"].reshape(num_chains, num_samples)), hdi_prob=0.9
+)
 
-az.plot_trace(np.array(discrete_samples["n"].reshape(num_chains,num_samples)));
+az.plot_trace(np.array(discrete_samples["n"].reshape(num_chains, num_samples)));
 
 
 # ### Continous `n`
@@ -93,29 +114,29 @@ az.plot_trace(np.array(discrete_samples["n"].reshape(num_chains,num_samples)));
 # > Actually, we don't necessary need to use DiscreteUniform for TotalN, as the computation of logp in Binomial doesn't require n to be an integer.
 
 def modelu(a, b, nmax, k=None):
-    u = numpyro.sample('u', dist.Uniform())
+    u = numpyro.sample("u", dist.Uniform())
     n = u * nmax
-    numpyro.deterministic('n', n)
-    th = numpyro.sample('th', dist.Beta(a, b))
+    numpyro.deterministic("n", n)
+    th = numpyro.sample("th", dist.Beta(a, b))
     size = len(k)
-    with numpyro.plate(f'i=1..{size}', size=size):
-        obs = numpyro.sample('k', dist.Binomial(total_count=n, probs=th), obs=k)
+    with numpyro.plate(f"i=1..{size}", size=size):
+        obs = numpyro.sample("k", dist.Binomial(total_count=n, probs=th), obs=k)
 
 
 # I guess the render doesn't track deterministic values...
-numpyro.render_model(modelu, (a,b,nmax,k), render_distributions=True)
+numpyro.render_model(modelu, (a, b, nmax, k), render_distributions=True)
 
 # + tags=[]
 kernel = NUTS(modelu)
-mcmc = MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains)
+mcmc = MCMC(
+    kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains
+)
 mcmc.run(keys[3], a=b, b=b, nmax=nmax, k=k)
 mcmc.print_summary()
 # -
 
-ds= az.from_numpyro(mcmc)
+ds = az.from_numpyro(mcmc)
 
 az.stats.summary(ds["posterior"]["n"], hdi_prob=0.9)
 
 az.plot_trace(ds);
-
-
